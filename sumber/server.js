93baa07551a6bbd5e39e39164c2bcf3e72234ac5
hartpoli <mdohart4@gmail.com>
2023-03-29 12:44:53 +0800

    commit semua

---
+++ sumber/server.js
@@ -0,0 +1,17 @@
+const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  })
+  server.route(routes)
+  await server.start()
+  console.log(`Server sedang berjalan di ${server.info.uri}`)
+}
+init()
